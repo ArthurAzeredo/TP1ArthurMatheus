bb2c95b679324a942d1d3aa52c41c249b14dd78e
ArthurAzeredo <arthursantos.azeredo1@gmail.com>
2019-11-07 10:58:57 -0200

    oi

--- TP1ArthurMatheus/src/main/java/br/edu/iff/TP1ArthurMatheus/servlets/Meu1oservlet.java
+++ TP1ArthurMatheus/src/main/java/br/edu/iff/TP1ArthurMatheus/servlets/Meu1oservlet.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.edu.iff.TP1ArthurMatheus;
+package br.edu.iff.TP1ArthurMatheus.servlets;
       //br.edu.iff.tp1_chagas.Meu1oservlet
 import java.io.IOException;
 import java.io.PrintWriter;
